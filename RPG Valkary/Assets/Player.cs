5366bec156d70cec8c7f1262968a876183e7a5c0
manikanta500 <manikanta@tharsha.com>
2023-07-15 00:09:00 +0530

    Added movement with idle and run animation

--- RPG Valkary/Assets/Player.cs
+++ RPG Valkary/Assets/Player.cs
@@ -7,11 +7,13 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 5;
     [SerializeField] private float jumpForce = 5;
-   private float xInput;
-   
+    private float xInput;
+    private bool isMoving;
+    private Animator anim;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
 
@@ -20,7 +22,10 @@ public class Player : MonoBehaviour
         xInput = Input.GetAxisRaw("Horizontal");
 
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-        if(Input.GetKeyDown(KeyCode.Space))
-         rb.velocity = new Vector2(rb.velocity.x,jumpForce );
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+        isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
     }
 }
